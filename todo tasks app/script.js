56622af051592215f5709fbbcdd77ea5181b1cb3
medhat1234 <121037476+medhat1234@users.noreply.github.com>
2023-12-15 20:15:57 +0200

    Add files via upload

---
+++ todo tasks app/script.js
@@ -0,0 +1,56 @@
+const inpt = document.getElementById("inpt");
+const btn = document.getElementById("btn");
+let tasks = [];
+//localStorage.clear();
+btn.onclick = (e) => add();
+setup();
+function add() {
+  if (!inpt.value) {
+    return;
+  }
+  tasks.push(inpt.value);
+  localStorage.setItem("tasks", `${tasks.join(",")}`);
+  createTask();
+}
+function setup() {
+  if (!localStorage.getItem("tasks")) {
+    return;
+  }
+  tasks = localStorage.getItem("tasks").split(",");
+
+  for (let i = 0; i < tasks.length; i++) {
+    reloadTasks(i);
+  }
+}
+function createTask() {
+  let div = document.createElement("div");
+
+  let delbutn = document.createElement("button");
+  let deltxt = document.createTextNode(`remove`);
+  delbutn.setAttribute("onclick", "remove(this)");
+  delbutn.appendChild(deltxt);
+  let txt = document.createTextNode(`${inpt.value}`);
+  div.appendChild(txt);
+  div.appendChild(delbutn);
+  document.body.appendChild(div);
+  inpt.value = "";
+}
+
+function reloadTasks(a) {
+  let div = document.createElement("div");
+  let delbutn = document.createElement("button");
+  let deltxt = document.createTextNode(`remove`);
+  delbutn.setAttribute("onclick", "remove(this)");
+  delbutn.appendChild(deltxt);
+  let txt = document.createTextNode(`${tasks[a]}`);
+  div.appendChild(txt);
+  div.appendChild(delbutn);
+  document.body.appendChild(div);
+}
+function remove(div) {
+  tasks = tasks.filter((i) => {
+    return !i === div.parentElement.innerText;
+  });
+  localStorage.setItem("tasks", `${tasks.join(",")}`);
+  div.parentElement.remove();
+}
